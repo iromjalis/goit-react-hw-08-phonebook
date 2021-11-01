2965982249e41b9c020ada81ce2fb7c50bdfc98d
Irina Romjalis <iromjalis@gmail.com>
2021-11-02 00:25:30 +0200

    image alt

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -6,14 +6,14 @@ const Home = () => {
       <p>
         <img
           src="https://lh3.googleusercontent.com/proxy/ZnMWOShsZvLauP_Xv-y-HNEkteChZiGf4-SjSwuEwmgOg6r62kpN8RwvYa8W9mChQhuAJV5ebhDGjXdllQhI-nxtTNP7YpRV4K0"
-          alt="homepage image"
+          alt="my phonebook"
           title="My phonebook page background"
         />
       </p>
       <p>
         <img
           src="https://3.imimg.com/data3/OP/FM/MY-8718020/police-telephone-directory-250x250.jpg"
-          alt="phonebook image"
+          alt="person with phone"
         />
       </p>
     </div>
